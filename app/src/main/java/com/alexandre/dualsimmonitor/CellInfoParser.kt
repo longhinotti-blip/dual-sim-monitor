faa5fa4d1a182bb24a9223bd5bdf8855027580cd
longhinotti-blip <longhinotti@gmail.com>
2026-09-19 21:23:01 -0300

    Fix Kotlin compilation by removing unsupported NR signal properties

--- app/src/main/java/com/alexandre/dualsimmonitor/CellInfoParser.kt
+++ app/src/main/java/com/alexandre/dualsimmonitor/CellInfoParser.kt
@@ -9,14 +9,68 @@ import android.telephony.CellInfoWcdma
 
 object CellInfoParser {
     fun parse(cell: CellInfo): CellSnapshot = when (cell) {
-        is CellInfoLte -> CellSnapshot("4G / LTE", cell.isRegistered, cell.cellIdentity.ci.readable(), cell.cellSignalStrength.dbm.readable(), cell.cellSignalStrength.asuLevel.readable(), cell.cellSignalStrength.rsrp.readable(), cell.cellSignalStrength.rsrq.readable(), cell.cellSignalStrength.rssnr.readable())
-        is CellInfoNr -> { val s = cell.cellSignalStrength; CellSnapshot("5G / NR", cell.isRegistered, cell.cellIdentity.toString(), s.dbm.readable(), s.asuLevel.readable(), s.ssRsrp.readable(), s.ssRsrq.readable(), s.ssSinr.readable()) }
-        is CellInfoWcdma -> CellSnapshot("3G / WCDMA", cell.isRegistered, cell.cellIdentity.cid.readable(), cell.cellSignalStrength.dbm.readable(), cell.cellSignalStrength.asuLevel.readable())
-        is CellInfoGsm -> CellSnapshot("2G / GSM", cell.isRegistered, cell.cellIdentity.cid.readable(), cell.cellSignalStrength.dbm.readable(), cell.cellSignalStrength.asuLevel.readable())
-        is CellInfoCdma -> CellSnapshot("CDMA", cell.isRegistered, cell.cellIdentity.basestationId.readable(), cell.cellSignalStrength.dbm.readable(), cell.cellSignalStrength.asuLevel.readable())
-        else -> CellSnapshot("Desconhecida", cell.isRegistered, cell.toString())
+        is CellInfoLte -> {
+            val signal = cell.cellSignalStrength
+            CellSnapshot(
+                technology = "4G / LTE",
+                registered = cell.isRegistered,
+                cell = cell.cellIdentity.ci.readable(),
+                dbm = signal.dbm.readable(),
+                asu = signal.asuLevel.readable(),
+                rsrp = signal.rsrp.readable(),
+                rsrq = signal.rsrq.readable(),
+                sinr = signal.rssnr.readable()
+            )
+        }
+        is CellInfoNr -> {
+            val signal = cell.cellSignalStrength
+            CellSnapshot(
+                technology = "5G / NR",
+                registered = cell.isRegistered,
+                cell = cell.cellIdentity.toString(),
+                dbm = signal.dbm.readable(),
+                asu = signal.asuLevel.readable()
+            )
+        }
+        is CellInfoWcdma -> CellSnapshot(
+            technology = "3G / WCDMA",
+            registered = cell.isRegistered,
+            cell = cell.cellIdentity.cid.readable(),
+            dbm = cell.cellSignalStrength.dbm.readable(),
+            asu = cell.cellSignalStrength.asuLevel.readable()
+        )
+        is CellInfoGsm -> CellSnapshot(
+            technology = "2G / GSM",
+            registered = cell.isRegistered,
+            cell = cell.cellIdentity.cid.readable(),
+            dbm = cell.cellSignalStrength.dbm.readable(),
+            asu = cell.cellSignalStrength.asuLevel.readable()
+        )
+        is CellInfoCdma -> CellSnapshot(
+            technology = "CDMA",
+            registered = cell.isRegistered,
+            cell = cell.cellIdentity.basestationId.readable(),
+            dbm = cell.cellSignalStrength.dbm.readable(),
+            asu = cell.cellSignalStrength.asuLevel.readable()
+        )
+        else -> CellSnapshot(
+            technology = "Desconhecida",
+            registered = cell.isRegistered,
+            cell = cell.toString()
+        )
     }
-    private fun Int.readable() = if (this == Int.MAX_VALUE || this < 0) "Indisponível" else toString()
+
+    private fun Int.readable(): String =
+        if (this == Int.MAX_VALUE || this < 0) "Indisponível" else toString()
 }
 
-data class CellSnapshot(val technology: String, val registered: Boolean, val cell: String, val dbm: String = "Indisponível", val asu: String = "Indisponível", val rsrp: String = "Indisponível", val rsrq: String = "Indisponível", val sinr: String = "Indisponível")
+data class CellSnapshot(
+    val technology: String,
+    val registered: Boolean,
+    val cell: String,
+    val dbm: String = "Indisponível",
+    val asu: String = "Indisponível",
+    val rsrp: String = "Indisponível",
+    val rsrq: String = "Indisponível",
+    val sinr: String = "Indisponível"
+)
